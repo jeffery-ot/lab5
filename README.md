32f8e9b1511ea39ea8a2c6ea9f1ac8cac8261f7b
jeffery-ot <jeffery.owusu@amalitechtraining.org>
2025-06-25 22:12:31 +0000

    fix: data model - deduplicated

---
+++ README.md
@@ -0,0 +1,10 @@
+## Project: Lakehouse Architecture for E-Commerce Transactions
+
+- local glue job to randomly generate raw data to s3 bucket
+- s3 bucket sends event to sqs on data arrival and triggers lambda function which invokes glue transformation job
+- glue job cleans, deduplicates and stores data as delta table file
+- glue crawls delta file for native and symlink metadata tables (2 crawlers needed for each table)
+-  implement logging and bad records
+- athena queries for table
+- orchestrate whole flow with step function
+- deploy with ci/cd github actions 
--- data_modelling/lab5_data_model.sql
+++ data_modelling/lab5_data_model.sql
@@ -12,28 +12,24 @@ CREATE TABLE "e_commerce_db"."department" (
 );
 
 CREATE TABLE "e_commerce_db"."users" (
-  "user_id" uuid UNIQUE PRIMARY KEY NOT NULL
+  "user_id" int PRIMARY KEY
 );
 
 CREATE TABLE "e_commerce_db"."orders" (
   "order_num" integer,
   "order_id" integer PRIMARY KEY,
-  "user_id" uuid,
+  "user_id" int,
   "order_timestamp" timestamp,
-  "total_amount" decimal(10,2),
-  "order_date" date
+  "total_amount" decimal(10,2)
 );
 
 CREATE TABLE "e_commerce_db"."order_items" (
   "id" integer PRIMARY KEY,
   "order_id" integer,
-  "user_id" uuid,
   "days_since_prior_order" int,
   "product_id" integer,
-  "add_to_cart_order" varchar,
-  "reordered" varchar,
-  "order_timestamp" timestamp,
-  "order_date" date
+  "add_to_cart_order" int,
+  "reordered" bool
 );
 
 ALTER TABLE "e_commerce_db"."product_data" ADD FOREIGN KEY ("department_id") REFERENCES "e_commerce_db"."department" ("department_id");
@@ -42,8 +38,4 @@ ALTER TABLE "e_commerce_db"."orders" ADD FOREIGN KEY ("user_id") REFERENCES "e_c
 
 ALTER TABLE "e_commerce_db"."order_items" ADD FOREIGN KEY ("order_id") REFERENCES "e_commerce_db"."orders" ("order_id");
 
-ALTER TABLE "e_commerce_db"."order_items" ADD FOREIGN KEY ("user_id") REFERENCES "e_commerce_db"."users" ("user_id");
-
 ALTER TABLE "e_commerce_db"."order_items" ADD FOREIGN KEY ("product_id") REFERENCES "e_commerce_db"."product_data" ("product_id");
-
-ALTER TABLE "e_commerce_db"."order_items" ADD FOREIGN KEY ("order_date") REFERENCES "e_commerce_db"."order_items" ("id");
